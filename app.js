389c3c627738efec770e2984326ee9e2cd0d7b84
web.guru@me.com <web.guru@me.com>
2019-10-04 14:43:57 +0530

    added user model and configure it with events to have a relation, also created new graphql mutations for creating a new user and editing createEvents to dynamically add the user as a cretor

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+const bcrypt = require('bcryptjs');
+
 const mongoose = require('mongoose');
 
 // Model
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -24,11 +27,22 @@ app.use(
       date: String!
     }
 
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
+
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+
+    input UserInput {
+      email: String!
+      password: String!
     }
 
     type RootQuery {
@@ -37,6 +51,7 @@ app.use(
 
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
 
     schema {
@@ -63,16 +78,57 @@ app.use(
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price, // converts to a float
-          date: new Date(args.eventInput.date)
+          date: new Date(args.eventInput.date),
+          creator: '5d970b25c583525b1455e71a'
         });
+
+        let createdEvent;
 
         return event
           .save()
           .then(result => {
-            return { ...result._doc, _id: result.id };
+            createdEvent = { ...result._doc, _id: result.id };
+
+            return User.findById('5d970b25c583525b1455e71a');
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error('User not found');
+            }
+
+            user.createdEvents.push(event);
+
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
           })
           .catch(err => {
             console.log(err);
+            throw err;
+          });
+      },
+
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error('User already exists');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, _id: result.id, password: null };
+          })
+          .catch(err => {
             throw err;
           });
       }
