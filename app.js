3782fcd2d5aa9db66f6dc591c0ec0cb85574a6b3
web.guru@me.com <web.guru@me.com>
2019-10-04 13:32:21 +0530

    Add events and create events type, query and mutation

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.use(
   '/graphql',
   graphqlHttp({
     schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -26,12 +43,22 @@ app.use(
   `),
     rootValue: {
       events: () => {
-        return ['Romantic', 'cooking', 'sailing', 'all night coding'];
+        return events;
       },
 
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price, // converts to a float
+          date: args.eventInput.date
+        };
+
+        console.log(event);
+
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
